c6f29b35ee2466c8b396657b0f3030657cdce5a7
joelgithire <jlmukono@gmail.com>
2022-08-07 19:11:13 +0300

    add cars list and cars item components

---
+++ src/components/CarItem/CarItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CarItem() {
+  return <div>CarItem</div>;
+}
+
+export default CarItem;
---
+++ src/components/CarsList/CarsList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CarItem from "../CarItem/CarItem";
+
+function CarsList() {
+  return (
+    <div>
+      CarsList
+      <CarItem></CarItem>
+    </div>
+  );
+}
+
+export default CarsList;
