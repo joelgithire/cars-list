a9f691d672e623c1f9668e3385a09e9ea288530b
joelgithire <jlmukono@gmail.com>
2022-08-08 20:30:48 +0300

    add box shadow to nav bar and car card

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <Nav />
       <CarsList />
+      <CarsList />
     </div>
   );
 }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,7 +3,7 @@ import "./Nav.css";
 function Nav() {
   return (
     <div className="nav-bar">
-      <div className="nav-title">CarsList</div>
+      <div className="nav-title">Cars-List</div>
       <div className="add-car">+ add Car</div>
     </div>
   );
