358cad777fadb2de2bcfd03f6ad3ff071073f5ce
joelgithire <jlmukono@gmail.com>
2022-08-09 11:01:08 +0300

    add props

--- src/components/CarItem/CarItem.js
+++ src/components/CarItem/CarItem.js
@@ -1,22 +1,13 @@
 import React from "react";
 import "./CarItem.css";
 
-function CarItem() {
+function CarItem(props) {
   return (
     <div className="car-item">
       <div className="car-image">
-        <img
-          alt="tesla"
-          src="https://tesla-cdn.thron.com/delivery/public/image/tesla/195458a0-ff67-488c-b972-14d23d2c42fb/bvlatuR/std/1200x630/ms-homepage-social"
-        />
+        <img alt="tesla" src={props.allCars.carImage} />
       </div>
-      <div className="car-description">
-        The Tesla Model 3 is a compact executive sedan that is battery powered
-        and produced by Tesla.[7] Limited production of the Model 3 began in
-        mid-2017, with the first production vehicle rolling off the assembly
-        line on July 7, 2017.[8][9][10] The official launch and delivery of the
-        first 30 cars took place on July 28.
-      </div>
+      <div className="car-description">{props.allCars.carDescription}</div>
       <div className="car-button">Button</div>
     </div>
   );
--- src/components/CarsList/CarsList.js
+++ src/components/CarsList/CarsList.js
@@ -2,10 +2,17 @@ import React from "react";
 import "./CarsList.css";
 import CarItem from "../CarItem/CarItem";
 
-function CarsList() {
+const DUMMUY_CAR_DATA = {
+  carImage:
+    "https://upload.wikimedia.org/wikipedia/commons/9/97/Lamborghini_Aventador_LP700-4_Orange.jpg",
+  carDescription:
+    "Automobili Lamborghini S.p.A. is an Italian brand and manufacturer of luxury sports cars and SUVs based in Sant'Agata Bolognese. The company is owned by the Volkswagen Group through its subsidiary Audi.",
+};
+
+function CarsList(props) {
   return (
     <div className="car-list">
-      <CarItem></CarItem>
+      <CarItem allCars={DUMMUY_CAR_DATA}></CarItem>
     </div>
   );
 }
