fa7f557d5301fdea44d6731e6c8a2ff8a6fbcffd
joelgithire <jlmukono@gmail.com>
2022-08-16 12:42:40 +0300

    add car name

--- src/components/CarItem/CarItem.js
+++ src/components/CarItem/CarItem.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CarItem.css";
 
 function CarItem(props) {
+  const [carName, setCarName] = useState("");
+  const setCarNameHandler = () => {
+    setCarName("Lambo");
+  };
+
   return (
     <div className="car-item">
       <div className="car-image">
+        <div className="car-name">{carName}</div>
         <img alt="tesla" src={props.allCars.carImage} />
       </div>
       <div className="car-description">{props.allCars.carDescription}</div>
-      <div className="car-button">Button</div>
+      <div className="car-button" onClick={setCarNameHandler}>
+        Button
+      </div>
     </div>
   );
 }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Nav.css";
 function Nav() {
+  const addCarHanlder = () => {
+    console.log("add car");
+  };
   return (
     <div className="nav-bar">
       <div className="nav-title">Cars-Rent</div>
       <div className="add-car">
-        <button>+ add Car</button>
+        <button onClick={addCarHanlder}>+ add Car</button>
       </div>
     </div>
   );
