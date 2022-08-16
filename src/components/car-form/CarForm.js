e09babf3419e1fe65d600a77c3aacfa91cd467fd
joelgithire <jlmukono@gmail.com>
2022-08-16 23:15:23 +0300

    get values from from

--- src/components/car-form/CarForm.js
+++ src/components/car-form/CarForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CarForm.css";
 
 const onChangeHanlder = (event) => {
@@ -6,22 +6,66 @@ const onChangeHanlder = (event) => {
 };
 
 function CarForm() {
+  const [carName, setCarName] = useState("");
+  const [carImageUrl, setCarImageUrl] = useState("");
+  const [carDescription, setCarDescription] = useState("");
+
+  const changeCarNameHandler = (event) => {
+    setCarName(event.target.value);
+  };
+  const changeCarImageUrlHandler = (event) => {
+    setCarImageUrl(event.target.value);
+  };
+  const changeCarDescriptionHandler = (event) => {
+    setCarDescription(event.target.value);
+  };
+
+  const onSubmitCarsHandler = (event) => {
+    event.preventDefault();
+    const enteredCars = {
+      carName: carName,
+      carImageUrl: carImageUrl,
+      carDescription: carDescription,
+    };
+    console.log(enteredCars);
+    clearFormInputs();
+  };
+
+  const clearFormInputs = () => {
+    setCarName("");
+    setCarImageUrl("");
+    setCarDescription("");
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmitCarsHandler}>
       <div className="car-form">
         <h1>Add car</h1>
         <div className="car-inputs">
           <div>
             <label>Enter Car Name</label>
-            <input onChange={onChangeHanlder} type="text" name="car name" />
+            <input
+              value={carName}
+              onChange={changeCarNameHandler}
+              type="text"
+              name="car name"
+            />
           </div>
           <div>
             <label>Enter Car Image Url</label>
-            <input type="text" name="car name" />
+            <input
+              value={carImageUrl}
+              onChange={changeCarImageUrlHandler}
+              type="text"
+              name="car name"
+            />
           </div>
           <div>
             <label>Enter Car Description</label>
-            <textarea></textarea>
+            <textarea
+              value={carDescription}
+              onChange={changeCarDescriptionHandler}
+            ></textarea>
           </div>
           <button>Add car</button>
         </div>
