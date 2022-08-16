b7357e617a4ed06beb75ace3d2477bfdc8cfeb47
joelgithire <jlmukono@gmail.com>
2022-08-16 22:26:22 +0300

    change input font size

--- src/components/car-form/CarForm.js
+++ src/components/car-form/CarForm.js
@@ -1,26 +1,32 @@
 import React from "react";
 import "./CarForm.css";
 
+const onChangeHanlder = (event) => {
+  console.log(event);
+};
+
 function CarForm() {
   return (
-    <div className="car-form">
-      <h1>Add car</h1>
-      <div className="car-inputs">
-        <div>
-          <label>Enter Car Name</label>
-          <input type="text" name="car name" />
+    <form>
+      <div className="car-form">
+        <h1>Add car</h1>
+        <div className="car-inputs">
+          <div>
+            <label>Enter Car Name</label>
+            <input onChange={onChangeHanlder} type="text" name="car name" />
+          </div>
+          <div>
+            <label>Enter Car Image Url</label>
+            <input type="text" name="car name" />
+          </div>
+          <div>
+            <label>Enter Car Description</label>
+            <textarea></textarea>
+          </div>
+          <button>Add car</button>
         </div>
-        <div>
-          <label>Enter Car Image Url</label>
-          <input type="text" name="car name" />
-        </div>
-        <div>
-          <label>Enter Car Description</label>
-          <textarea></textarea>
-        </div>
-        <button>Add car</button>
       </div>
-    </div>
+    </form>
   );
 }
 
