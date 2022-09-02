6cb3cc631c69c90007cde132260673c1b1d1b4ba
joelgithire <jlmukono@gmail.com>
2022-09-02 22:17:42 +0300

    remove button

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,10 @@
 import React from "react";
 import "./Nav.css";
 function Nav() {
-  const addCarHanlder = () => {
-    console.log("add car");
-  };
   return (
     <div className="nav-bar">
       <div className="nav-title">Cars-List</div>
-      <div className="add-car">
-        <button onClick={addCarHanlder}>+ add Car</button>
-      </div>
+      <div className="add-car"></div>
     </div>
   );
 }
